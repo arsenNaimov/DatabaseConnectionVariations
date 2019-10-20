c903aa322751c2b776fea47f517ecb39c857ca97
Арсен <arsennaimov@gmail.com>
2019-10-20 14:34:16 +0300

    add hibernate

--- src/main/java/ClassForTestBd.java
+++ src/main/java/ClassForTestBd.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Domain {
+public class ClassForTestBd {
 
     public static void main(String[] args) throws SQLException {
         AddressService addressService = new AddressService();
